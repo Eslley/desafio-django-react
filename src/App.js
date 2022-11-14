88ad2e79eb9375cc9a28103ea0c34d61c0edf0ef
Eslley <rubemeslley@gmail.com>
2022-11-14 19:37:58 -0300

    Iniciando projeto React

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+function App() {
+
+  useEffect(() => {
+    axios.post('http://127.0.0.1:8000/usuarios/', {
+      name: "teste",       //sending data to backend
+      description: "teste"
+    })
+      .then(response => {
+        console.log(response.data)    // view the response
+      })
+      .catch(error => {
+        console.log(error)            // check if any error
+      })
+  }, [])
+
+  return (
+    <h1>Hello</h1>
+  );
+}
+
+export default App;
