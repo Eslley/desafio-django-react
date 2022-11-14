7eefd516599f2dcfef291f63b8c8ba5978726bf0
Eslley <rubemeslley@gmail.com>
2022-11-14 20:37:51 -0300

    fix: Redirecionando para index.html

--- challenge/urls.py
+++ challenge/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuarios/', include('api.urls'))
+    path('usuarios/', include('api.urls')),
+    re_path('.*',TemplateView.as_view(template_name='index.html'))
 ]
